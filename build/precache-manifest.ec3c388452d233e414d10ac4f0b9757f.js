f4cdfdb59f71ca62a3459b9a51068e802ac1bdc2
wanglin111 <273215924@qq.com>
2020-10-18 13:02:29 +0800

    task2

---
+++ build/precache-manifest.ec3c388452d233e414d10ac4f0b9757f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8f79a6d612ddcbe490cbe51ba28b4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a83552d5734b56754e75",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "6f28a6543455cc9e2a9b",
+    "url": "/static/css/main.c727e945.chunk.css"
+  },
+  {
+    "revision": "a83552d5734b56754e75",
+    "url": "/static/js/2.8ab8949a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8ab8949a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f28a6543455cc9e2a9b",
+    "url": "/static/js/main.0ebf63a5.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
